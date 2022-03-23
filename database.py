a8584f59de8a2fd148179d03b9bc2d81e36f6818
Omar-Zantot <74403682+Omar-Zantot@users.noreply.github.com>
2022-03-23 20:17:59 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,12 @@
 # Osama 
+import pandas as pd
+class Databae():
+    def Check_cridt_card(self,Search_key):
+        df = pd.read_excel('PayLeaks.xlsx')
+        if(len(Search_key) == 11):
+            return df.loc[df['PayLeaks'] == Search_key]
+        else:
+            return "invalid input"
+
+OPJ=Databae()
+print(OPJ.Check_cridt_card("40604-30720"))
