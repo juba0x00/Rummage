fe5cc24b741c24eeb8c042f3c4b116900d382d23
silentco2 <92058267+silentco2@users.noreply.github.com>
2022-04-21 05:14:14 +0200

    Update database.py

--- database.py
+++ database.py
@@ -23,5 +23,42 @@ class Database:
             return False
 
 
+#Egypt
+import sqlite3 as sql
+conn = sql.connect('Leaks.db')
+cursor = conn.cursor()
 
 
+def create_table(table_name, attr):
+    cursor.execute(f"Create table {table_name} ({attr}); ")
+
+
+def insert_data(table_name, data1):
+    cursor.execute(f"insert into {table_name} values('{data1}');")
+
+
+def add_row(history, main, value):
+    cursor.execute(f" insert into {history} select num from {main} where num='+2{value}';")
+
+
+def exist(table_name, value):
+    return cursor.execute(f"Select * from {table_name} where num = '+2{value}';").fetchone() is not None
+
+
+def search(history, main, value):
+    if exist(history, value):
+        return 1
+    if exist(main, value):
+        add_row(history, main, value)
+        return 1
+    return 0
+
+
+# def insert_data_file(filename, table_name):
+#     with open(filename) as f:
+#         file_data = f.readlines()
+#         [insert_data(table_name, value[value.find('+'):value.find('"+')+15].strip('"')) for value in file_data]
+
+
+# def show_result(table_name):
+#     print(cursor.execute(f"Select * from {table_name} ;").fetchall())
