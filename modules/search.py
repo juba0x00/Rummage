39c8be5a9422b48a5d9b10c16d1558eab2c00dd1
Juba0x4355 <mahmoudhawash90@gmail.com>
2022-04-23 11:48:20 +0200

    Multi-Threading

--- modules/search.py
+++ modules/search.py
@@ -8,88 +8,54 @@ from modules.validateinput import CheckinputType
 from os import getcwd
 import threading 
 from time import sleep
-# 
+
 class Search():
+        
+    def __init__(self):
+        self.__CWD = getcwd()
+    def __EmailSearch(self):
+        self.BreachDirFinder = BreachDir()
+        self.ScatterFinder = ScatterSecrets()
+        self.DarkFinder = DarkNet()
+        
+        t1 = threading.Thread(target=self.ScatterFinder.Search())
+        t2 = threading.Thread(target=self.BreachDirFinder.Search())
+        t3 = threading.Thread(target=self.DarkFinder.Search())
+        threads = [t1, t2, t3]
+        
+        for thread in threads:
+            thread.start()
+        for thread in threads:
+            thread.join()
+            
+            
     
+    def __UsernameSearch(self):
+        t1 = threading.Thread(target=self.BreachDirFinder.Search())
+        t2 = threading.Thread(target=self.DarkFinder.Search())
+        t2.start()
+        t1.start()
+        for i in [t1, t2]:
+            i.join()
 
     def Search(self, SearchKey):
         SearchType = CheckinputType(SearchKey)
-        LeaksFinder.SetAtrrs(SearchKey, SearchType) 
+        LeaksFinder.SetAtrrs(SearchKey, SearchType)
         
-        # self.DbFinder.Search('History')
+        # self.DatabaseFinder = Database()
+        # self.DatabaseFinder.Search()  # ? History Search Default Database name
         
         if SearchType == 'Email':
+            self.__EmailSearch()
 
-            # self.BreachDirFinder.Search()
-            self.ScatterFinder = ScatterSecrets()
-            self.ScatterFinder.Search()
-
-            self.DarkFinder = DarkNet()
-            
-            self.DarkFinder.Search()
-            
         elif SearchType == 'Username':
-            # self.BreachDirFinder.Search()
-            self.DarkFinder.Search()
-            
-        elif SearchType == 'PhoneNumber':
-            pass
-            # self.DbFinder.Search(Internet.GetCountry)
-            
-        elif SearchType == 'Visa':
-            pass
-            # self.DbFinder.Search('Visa')
-        else:
-            #! window error 
-            pass
-        
-# class Search():
-        
-#     def __init__(self):
-#         self.__CWD = getcwd()
-    # def __EmailSearch(self):
-    #     self.BreachDirFinder = BreachDir()
-    #     self.ScatterFinder = ScatterSecrets()
-    #     self.DarkFinder = DarkNet()
-        
-    #     t1 = threading.Thread(target=self.ScatterFinder.Search())
-    #     t2 = threading.Thread(target=self.BreachDirFinder.Search())
-    #     t3 = threading.Thread(target=self.DarkFinder.Search())
-    #     threads = [t1, t2, t3]
-        
-    #     for thread in threads:
-    #         thread.start()
-    #     for thread in threads:
-    #         thread.join()
-            
-            
-    
-    # def __UsernameSearch(self):
-    #     t1 = threading.Thread(target=self.BreachDirFinder.Search())
-    #     t2 = threading.Thread(target=self.DarkFinder.Search())
-    #     t2.start()
-    #     t1.start()
-    #     for i in [t1, t2]:
-    #         i.join()
-
-    # def Search(self, SearchKey):
-    #     SearchType = CheckinputType(SearchKey)
-    #     LeaksFinder.SetAtrrs(SearchKey, SearchType)
-        
-    #     # self.DatabaseFinder = Database()
-    #     # self.DatabaseFinder.Search()  # ? History Search Default Database name
-        
-    #     if SearchType == 'Email':
-    #         self.__EmailSearch()
-
-    #     elif SearchType == 'Username':
-    #         self.__UsernameSearch()
+            self.__UsernameSearch()
                 
-    #     # elif SearchType == 'PhoneNumber':
-    #     #     self.DatabaseFinder.Search(Internet.GetCountry)
+        # elif SearchType == 'PhoneNumber':
+        #     self.DatabaseFinder.Search(Internet.GetCountry)
             
-    #     # elif SearchType == 'Visa':
-    #     #     self.DatabaseFinder.Search('Visa')
-    #     else:
-    #         # ! window error 
-    #         pass 
+        # elif SearchType == 'Visa':
+        #     self.DatabaseFinder.Search('Visa')
+        else:
+            # ! window error 
+            pass 
